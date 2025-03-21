603fe15499959418a49ce7c0a6b49509cbb2da36
lgw323 <67824730+lgw323@users.noreply.github.com>
2025-03-21 20:50:49 +0900

    3주차 파일 업로드

---
+++ week03/myname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+        int id;
+        char name[30];
+        scanf("%d %s", &id, name);
+	printf("ìí•í•ID: %d\n", id);
+	printf("NAME: %s\n", name);
+	return 0;
+}
