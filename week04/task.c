fb883af4e86fdb169199402cd280665cfd136b83
lgw323 <67824730+lgw323@users.noreply.github.com>
2025-03-29 21:49:24 +0900

    4주차 실습 과제 내용 업데이트

--- week04/task.c
+++ week04/task.c
@@ -10,8 +10,16 @@ int main()
         if(word == '0') {break;}
         else
         {
-            char little_word = (char)(word + 32);
-            printf("%c의 소문자는 %c입니다.\n", word, little_word);
+            if('a' <= word && word <= 'z')
+            {
+                char little_word = (char)(word - 32);
+                printf("%c의 대문자는 %c입니다.\n", word, little_word);
+            }
+            else if('A' <= word && word <= 'Z')
+            {
+                char little_word = (char)(word + 32);
+                printf("%c의 소문자는 %c입니다.\n", word, little_word);
+            }
         }
     }
     return 0;
